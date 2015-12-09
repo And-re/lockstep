72e5e6fdd082d6c6e7b71b3df9c5957a06052b97
mbp <przemek@MacBook-Pro-mbp.local>
2015-12-09 17:14:09 +0100

    Add tasklist date filtering

--- client/Components/TasksLog.jsx
+++ client/Components/TasksLog.jsx
@@ -1,6 +1,12 @@
 TasksLog = React.createClass({
     mixins: [ReactMeteorData],
 
+    getInitialState() {
+        return {
+            selectedDate: false
+        }
+    },
+
     getMeteorData() {
         let users;
 
@@ -15,38 +21,66 @@ TasksLog = React.createClass({
         let _tasks = Tasks.find({type: {$in: ['planned', 'completed']}}, {sort: {createdAt: 1}}).fetch();
         let taskDates = distinctDates(_tasks);
 
+        let _query = {};
+
+        if (this.state.selectedDate) {
+            let _clickedDateMidnight1 = moment(this.state.selectedDate, 'YYYY/MM/DD').startOf('day').valueOf();
+            let _clickedDateMidnight2 = moment(this.state.selectedDate, 'YYYY/MM/DD').endOf('day').valueOf();
+
+            _query = {
+                "startTime": {
+                    "$gte": _clickedDateMidnight1,
+                    "$lt": _clickedDateMidnight2
+                }
+            }
+        }
+
         return {
             users: users,
             taskDates: taskDates,
-            plannedTasks: Tasks.find({type: 'planned'}, {sort: {createdAt: 1}}).fetch(),
-            completedTasks: Tasks.find({type: 'completed'}, {sort: {createdAt: 1}}).fetch()
+            plannedTasks: Tasks.find(_.extend({type: 'planned'}, _query), {sort: {createdAt: 1}}).fetch(),
+            completedTasks: Tasks.find(_.extend({type: 'completed'}, _query), {sort: {createdAt: 1}}).fetch()
         };
     },
 
+    filterDate(event) {
+        let _selectedDate = event.target.innerText;
+
+        this.setState({
+            selectedDate: (this.state.selectedDate === _selectedDate) ? false : _selectedDate
+        });
+    },
+
     render() {
+        let dateClasses = function (that, date) {
+            return classNames({
+                'active': that.state.selectedDate === date
+            })
+        };
+
         return (
             <div className="row">
                 <div className="col-md-12">
                     <ul className="pagination pagination-sm">
                         {this.data.taskDates.map((date) => {
                             return (
-                                <li key={date}>
-                                    <a href="#">{date}</a>
-                                </li>
-                            );
-                        })}
+                            <li className={dateClasses(this, date)} key={date} onClick={this.filterDate}>
+                                <a href="#">{date}</a>
+                            </li>
+                                );
+                            })}
                     </ul>
                 </div>
                 <div className="col-md-6">
                     <div className="panel panel-primary">
                         <div className="panel-heading">Completed Tasks ({this.data.completedTasks.length})</div>
-                        <TaskList tasks={this.data.completedTasks} users={this.data.users} />
+                        <TaskList tasks={this.data.completedTasks} users={this.data.users}/>
                     </div>
                 </div>
                 <div className="col-md-6">
                     <div className="panel panel-primary">
                         <div className="panel-heading">Planned Tasks ({this.data.plannedTasks.length})</div>
-                        <TaskList tasks={this.data.plannedTasks} users={this.data.users} />
+                        <TaskList tasks={this.data.plannedTasks} users={this.data.users}/>
                     </div>
                 </div>
             </div>
@@ -58,7 +92,17 @@ function distinctDates(data) {
     let dates = [];
 
     data.forEach((doc) => {
-        dates.push(moment(doc.createdAt).format('YYYY/MM/DD'));
+        dates.push(moment(doc.startTime).format('YYYY/MM/DD'));
+    });
+
+    return _.uniq(dates);
+}
+
+function distinctPhasesStartTime(data) {
+    let dates = [];
+
+    data.forEach((doc) => {
+        dates.push(moment(doc.startTime));
     });
 
     return _.uniq(dates);
